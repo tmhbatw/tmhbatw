2da7891a870400bceb9ca84babd96f55433d8636
tmhbatw <1060226998@qq.com>
2022-09-06 23:11:27 +0800

    Initial commit

---
+++ src/main/java/com/tmhbatw/tmhbatw/TmhbatwApplication.java
@@ -0,0 +1,13 @@
+package com.tmhbatw.tmhbatw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmhbatwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmhbatwApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/tmhbatw/tmhbatw/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.tmhbatw.tmhbatw.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String Hello(){
+        return "hello world!";
+    }
+
+}
