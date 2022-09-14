931afb9236b4f863cab2786e2d9668de6ebec85d
liuruihong.1 <liuruihong.1@bytedance.com>
2022-09-14 13:39:27 +0800

    更新了破烂系统的登陆功能

--- src/main/java/com/tmhbatw/tmhbatw/controller/LoginController.java
+++ src/main/java/com/tmhbatw/tmhbatw/controller/LoginController.java
@@ -1,5 +1,29 @@
 package com.tmhbatw.tmhbatw.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class LoginController {
 
+    @GetMapping("/login")
+    public Map<String,Boolean> login(@RequestParam Map<String,String> map) {
+        String name =  map.getOrDefault("name","");
+        String password = map.getOrDefault("password", "");
+
+        System.out.println(map);
+
+        Map<String,Boolean> result=new HashMap<>();
+        if(name.equals("tmhbatw")&&password.equals("shuai")){
+            result.put("result",true);
+        }else{
+            result.put("result",false);
+        }
+        return result;
+    }
 }
